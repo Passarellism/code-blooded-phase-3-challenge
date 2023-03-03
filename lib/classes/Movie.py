257d61ded9bd13d5e68a04f1a450a7ac40dc889a
steve passarelli <passarellism@gmail.com>
2023-03-03 11:51:04 -0600

    complete challenge, 1 failing test

--- lib/classes/Movie.py
+++ lib/classes/Movie.py
@@ -3,19 +3,29 @@ from classes.Review import Review
 
 class Movie:
     def __init__(self, title):
-        pass
+        self.title = title
 
-    # title property goes here!
+    def get_title(self):
+        return self._title
+    
+    def set_title(self, title):
+        if isinstance(title, str) and 0 < len(title):
+            self._title = title
+
+    title = property(get_title, set_title)
 
     def reviews(self):
-        pass
+        return [review for review in Review.all if review.movie == self]
 
     def reviewers(self):
-        pass
+        return [review.viewer for review in self.reviews()]
 
     def average_rating(self):
-        pass
+        avg_rating = [review.rating for review in self.reviews()]
+        return sum(avg_rating) / len(avg_rating)
 
-    @classmethod
-    def highest_rated(cls):
-        pass
+
+    # @classmethod
+    # def highest_rated(cls):
+    #     return max(movie in cls.average_rating() if )
+            
